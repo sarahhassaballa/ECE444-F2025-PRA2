7cf28a10c91a3bce806ed0cc59a9e3b6fa650baa
Sarah Hassaballa <sarahhassaballa@hotmail.com>
2025-09-20 00:13:36 -0400

    Added Example 2-2 code.

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
-# Example 2-1
+# Example 2-1 & 2-2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello {}!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run()

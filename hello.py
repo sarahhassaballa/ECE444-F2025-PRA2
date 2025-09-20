f88aea7ef64f5d0b4199693a8535968d7724925f
Sarah Hassaballa <sarahhassaballa@hotmail.com>
2025-09-20 00:19:25 -0400

    Updated Example 2-2 code.

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello {}!</h1>'.format(name)
+    return '<h1>Hello, {}!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run()

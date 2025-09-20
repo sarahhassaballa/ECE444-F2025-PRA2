1db7cecfa6ae4cef98c94f235e3743073ffec32e
Sarah Hassaballa <sarahhassaballa@hotmail.com>
2025-09-20 00:56:10 -0400

    Added Example 3-4 code.

--- hello.py
+++ hello.py
@@ -13,9 +13,12 @@ def user(name):
 if __name__ == '__main__':
     app.run()
 '''
-#Example 3-3
+#Example 3-4
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():

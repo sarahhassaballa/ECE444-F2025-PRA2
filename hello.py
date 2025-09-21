817989c1d6bb30c424684dea9ddd33bdc5c15bdb
Sarah Hassaballa <sarahhassaballa@hotmail.com>
2025-09-21 00:53:40 -0400

    Example 4-7 code added.

--- hello.py
+++ hello.py
@@ -20,17 +20,40 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 #Example 3-13
 from datetime import datetime
+#Example 4-2
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SubmitField 
+from wtforms.validators import DataRequired
+#Example 4-6
+from flask import Flask, render_template, session, redirect, url_for, flash
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-#Example 3-13
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+class NameForm (FlaskForm): 
+    name = StringField('What is your name?', validators=[DataRequired()]) 
+    submit = SubmitField('Submit')
+
+#Example 4-6
+@app.route('/', methods=['GET', 'POST']) 
+def index(): 
+    form = NameForm() 
+    if form.validate_on_submit(): 
+        old_name = session.get('name') 
+        if old_name is not None and old_name != form.name.data: 
+            flash('Looks like you have changed your name!') 
+        session['name'] = form.name.data 
+        return redirect(url_for('index')) 
+    return render_template('index.html', current_time=datetime.utcnow(),
+        form = form, name = session.get('name'))
+
+#Example 3-13 
+#@app.route('/')
+#def index():
+#    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
